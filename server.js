3a6aff2d7fdfe8b16d2ee961f822e2ed2a02d351
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2018-09-17 20:55:58 -0500

    On branch master  Changes to be committed: 	modified:   .gitignore 	new file:   media/static/images/restaurantic_img_1_1920x1080.jpg 	new file:   package.json 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,60 @@
+var StaticServer = require('static-server');
+var server = new StaticServer({
+  rootPath: './media/static/',            // required, the root of the server file tree
+  port: 1337,               // required, the port to listen
+  name: 'media-static-server',   // optional, will set "X-Powered-by" HTTP header
+  //host: '10.0.0.100',       // optional, defaults to any interface
+  host: '127.0.0.1',
+  cors: '*',                // optional, defaults to undefined
+  followSymlink: true,      // optional, defaults to a 404 error
+  templates: {
+    index: 'foo.html',      // optional, defaults to 'index.html'
+    notFound: '404.html'    // optional, defaults to undefined
+  }
+});
+ 
+server.start(function () {
+  console.log('Server listening to', server.port);
+});
+ 
+server.on('request', function (req, res) {
+  // req.path is the URL resource (file name) from server.rootPath
+  // req.elapsedTime returns a string of the request's elapsed time
+});
+ 
+server.on('symbolicLink', function (link, file) {
+  // link is the source of the reference
+  // file is the link reference
+  console.log('File', link, 'is a link to', file);
+});
+ 
+server.on('response', function (req, res, err, file, stat) {
+  // res.status is the response status sent to the client
+  // res.headers are the headers sent
+  // err is any error message thrown
+  // file the file being served (may be null)
+  // stat the stat of the file being served (is null if file is null)
+ 
+  // NOTE: the response has already been sent at this point
+});
+
+/*
+const { NodeMediaServer } = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+      },
+      http: {
+        port: 8000,
+        allow_origin: '*'
+    }
+};
+
+var nms = new NodeMediaServer(config);
+nms.run();
+*/
