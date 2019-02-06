177b0ed9b4ff5dd56c7bf19741cc4377ad68d8cb
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2019-02-05 23:36:44 -0500

    On branch master: se mejora server.js
    Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+var fs = require('fs');
 const express = require("express");
 const fileUpload = require('express-fileupload');
 const moment = require("moment-timezone");
@@ -17,6 +19,9 @@ app.post("/upload", async (req, res) => {
   if (req.get("x-access-key") !== "MAJE@O93I2G5#XX074*!") {
     return await res.status(401).send("Wrog key or not suplied.");
   }
+  if (!fs.existsSync("/app/files")){
+    fs.mkdirSync("/app/files");
+  }
   try {
     if (Object.values(req.files).length > 1){
       return await res.send("Error maximum number of files.");
@@ -28,7 +33,7 @@ app.post("/upload", async (req, res) => {
     const next = secs.get(key) + 1;
     secs.set(key, next);
     const listFileName = req.files.file.name.split(".");
-    await req.files.file.mv(`./files/${next.toString()}.${listFileName[listFileName.length - 1]}`);
+    await req.files.file.mv(`/app/files/${next.toString()}.${listFileName[listFileName.length - 1]}`);
     await res.send("uploaded.");
   } catch (err) {
     await res.send("Error processing file. " + err.message);
